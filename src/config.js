cdd0f4c376218145c565d36c451b30f228ecb025
josngel <cj_angel@outlook.com>
2021-11-17 19:15:58 -0500

    documentacion

--- src/config.js
+++ src/config.js
@@ -6,6 +6,6 @@ dotenv();
 export const config = {
     host: process.env.DB_HOST ||'localhost',
     user: process.env.DB_USER ||'root',
-    password: process.env.DB_PASSWORD ||'',
-    database: process.env.DB_DATABASE ||'tasks'
+    password: process.env.DB_PASSWORD ||'root',
+    database: process.env.DB_DATABASE ||'tasksdb'
 }
